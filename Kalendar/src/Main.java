9bd059cbb3dabfd02a1ceafba660e91a18379816
Badrillox <56489878+Badrillox@users.noreply.github.com>
2020-09-23 15:26:25 +0200

    Alle Dateien raufgepushed

---
+++ Kalendar/src/Main.java
@@ -0,0 +1,15 @@
+/*
+
+Wir brauchen ein Programm, dass das Datum und deren Feiertage ausließt und wird somit bestimmen können, wann man
+freinehmen sollte, um möglichst lange frei zu haben
+
+Ideen: Wir haben ein Array mit all den Feiertagen
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Kalendar/src/models/CurrentDate.java
@@ -0,0 +1,9 @@
+package models;
+import java.util.Date;
+public class CurrentDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+
+}
