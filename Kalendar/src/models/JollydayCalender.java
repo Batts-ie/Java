4f8a0d952f4b34fcd6ae40b476c42ca284bbc281
Badrillox <56489878+Badrillox@users.noreply.github.com>
2020-09-29 23:13:58 +0200

    Alle Dateien raufgepushed

--- Kalendar/src/models/JollydayCalender.java
+++ Kalendar/src/models/JollydayCalender.java
@@ -1,7 +1,7 @@
-package models;
+/*package models;
 import de.jollyday.*;
 
-import java.time.DayOfWeek;
+/*import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -9,12 +9,12 @@ public class JollydayCalender {
 
     public DayOfWeek cal(HolidayCalendar state, int years){
         years = Math.abs(years);
-
+        // Array erstellen für die Tage
         Holiday[] holidays = new Holiday[]{};
-
         HolidayManager man = HolidayManager.getInstance(ManagerParameters.create(state));
+        // von jetzt bis die Jahre, die oben als Parameter benötigt werden
         holidays = man.getHolidays(LocalDate.now(), LocalDate.now().plusYears(years)).toArray(new Holiday[0]);
 
         return DayOfWeek.SUNDAY;
     }
-}
+}*/
--- Kalendar/src/models/Menu.java
+++ Kalendar/src/models/Menu.java
@@ -1,9 +1,57 @@
 package models;
 
+import java.util.Scanner;
+
 public class Menu {
 
+    public static Scanner Reader = new Scanner(System.in);
 
+    public static void main(String[] args) {
 
+        char wahl = SelectionPool();
 
+        switch (wahl){
+            case 'y':
+                /*case 'O':
+                    //execution of O here
+                    break;
+                case 'D' :
+                    //execution of D here
+                    break;
+                case 'S':
+                    //execution of S here
+                    break;*/
+                break;
+            case 'x':
+                System.out.println("Programm wird wieder geschlossen, schönen Tag noch! ");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println("Programm konnte keine 2 sec warten");
+                }
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Falsche Taste betätigt, bitte das Programm neustarten und aufpassen");
+        }
+
+    }
+    public static char SelectionPool(){
+        char c;
+        System.out.println("Was wollen Sie tun?: ");
+        System.out.println("Öffne Kalender [y]: ");
+        System.out.println("Schließen [x]: ");
+        c = Reader.next().charAt(0);
+        return c;
+    }
+   /* public static char CountryPool(){
+        char c;
+        System.out.println("Geben Sie ein Land ein, aber aufpassen, Buchstaben müssen groß sein: ");
+        System.out.println("Österreich [O]: ");
+        System.out.println("Deutschland [D]: ");
+        System.out.println("Schweiz [S]: ");
+        c = Reader.next().charAt(0);
+        return c;
+    }*/
 
 }
