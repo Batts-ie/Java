5b14dd06ff961065054672596a7852a387b066fa
Badrillox <56489878+Badrillox@users.noreply.github.com>
2020-10-06 21:11:00 +0200

    WebRequest update

--- Calendar/src/models/WebRequest.java
+++ Calendar/src/models/WebRequest.java
@@ -1,19 +1,26 @@
 package models;
 
+import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class WebRequest {
-
+    String _requestString = "https://deutsche-feiertage-api.de/api/v1";
     public void getWebRequest(){
-        String requestString = "";
+        
         HttpURLConnection connection = null;
         try {
-            URL url = new URL(requestString);
+            URL url = new URL(_requestString);
             connection =(HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setUseCaches(false);
             connection.setDoOutput(true);
+            connection.setRequestProperty("", "X-DFA-Token: dfa");
+
+            // sending a request
+            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+            dos.writeBytes(null);
+            dos.close();
 
         } catch (Exception e) {
             System.out.println("Der requestString kann entweder nicht gefunden werden, oder nicht geöffnet werden");
