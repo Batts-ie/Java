a3becf4b8b8189641e79494e4af95b03c9ab2507
Badrillox <micperktold@tsn.at>
2020-12-02 21:08:54 +0100

    DB fertig mit Insert und Create Statements - WebRequest

--- Aktien/src/DB.java
+++ Aktien/src/DB.java
@@ -5,7 +5,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 public class DB{
 
-    public static void Connection(){
+    public static void Connection()
+    {
         String hostname = "localhost";
         String dBPort = "3306";
         String userName = "root";
@@ -13,7 +14,8 @@ public class DB{
 
         Connection con;
 		 
-        try{
+        try
+        {
             Class.forName("org.mariadb.jdbc.Driver");
         }
         catch(Exception e)
@@ -21,39 +23,47 @@ public class DB{
             System.out.println("Treiber konnte nicht richtig geladen werden ... ");
             e.printStackTrace();
         }
-        try {
+        try
+        {
                con = DriverManager.getConnection("jdbc:mariadb//"+hostname+":"+dBPort+"/"+"?user="+userName+"&password="+password+"&serverTimezone=UTC");
                Statement statement = con.createStatement();
              /*  ResultSet rS=statement.executeQuery("Select * from "); */
 
                con.close();
-           }
-           catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
                e.printStackTrace();
-           }
+        }
     }
 
-    public static void InsertStatement(Connection con, String key, String symbol, double number){
+    public static void InsertStatement(Connection con, String key, String symbol, double number)
+    {
         String insertInTable =  "INSERT OR REPLACE INTO "+symbol+" VALUES('"+key+"', "+number+");";
 
-        try{
+        try
+        {
             Statement stm = con.createStatement();
             stm.execute(insertInTable);
         }
-        catch (SQLException e){
+        catch (SQLException e)
+        {
             System.out.println("Die Tabelle konnte die Values nicht aufnehmen");
             e.printStackTrace();
         }
     }
 
-    public static void CreateTable(Connection con, String symbol){
+    public static void CreateTable(Connection con, String symbol)
+    {
         String createTable = "CREATE TABLE IF NOT EXISTS"+symbol+"(DATE CHAR(10) PRIMARY KEY, VALUE DOUBLE);";
 
-        try{
+        try
+        {
             Statement stm = con.createStatement();
             stm.execute(createTable);
         }
-        catch (SQLException e){
+        catch (SQLException e)
+        {
             System.out.println("Die Tabelle konnte nicht erzeugt werden");
             e.printStackTrace();
         }
